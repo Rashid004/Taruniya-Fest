2e38c1254d3972ff024090a9b37f7074cf4a3722
Ansari Rashid <rashidansari3868038@gmail.com>
2024-12-17 13:17:25 +0530

    seo

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,232 +1,242 @@
 /** @format */
 
 import { motion } from "framer-motion";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <section className="relative min-h-[90svh] w-full  overflow-hidden">
-      {/* Right Cloud */}
-      <motion.div
-        className="absolute top-[8vh] md:top-[10vh] lg:top-[8vh] right-0  w-[35vw] max-w-[300px] min-w-[180px]"
-        initial={{ x: 200, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 2.5,
-        }}
-      >
-        <img
-          src="/home/rightcloud.webp"
-          alt="Cloud 1"
-          className="hidden lg:block w-full"
-        />
-        <img
-          src="/home/cloudrightMobile.svg"
-          alt="Cloud 2"
-          className="block lg:hidden w-full "
-        />
-      </motion.div>
+    <HelmetProvider>
+      <section className="relative min-h-[90svh] w-full  overflow-hidden">
+        <Helmet>
+          <title>Tarunya Fest</title>
+          <meta
+            name="description"
+            content="Experience the magic of Tarunya Fest – a vibrant celebration of creativity, talent, and innovation. Explore events, competitions, workshops, and unforgettable performances. Join us for an extraordinary journey!"
+          />
+        </Helmet>
+        {/* Right Cloud */}
+        <motion.div
+          className="absolute top-[8vh] md:top-[10vh] lg:top-[8vh] right-0  w-[35vw] max-w-[300px] min-w-[180px]"
+          initial={{ x: 200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 2.5,
+          }}
+        >
+          <img
+            src="/home/rightcloud.webp"
+            alt="Cloud 1"
+            className="hidden lg:block w-full"
+          />
+          <img
+            src="/home/cloudrightMobile.svg"
+            alt="Cloud 2"
+            className="block lg:hidden w-full "
+          />
+        </motion.div>
 
-      {/* Left Cloud Mobile */}
-      <motion.div
-        className="absolute top-[18vh] md:top-[8vh] lg:top-[2vh] left-0  w-[35vw] max-w-[300px] min-w-[180px]"
-        initial={{ x: -200, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 2.7,
-        }}
-      >
-        <img
-          src="/home/cloudLeftMobile.svg"
-          alt="Cloud Mobile 1"
-          className="block lg:hidden w-full"
-        />
-        <img
-          src="/home/leftcloud.webp"
-          alt="Cloud 1"
-          className="hidden lg:block w-full"
-        />
-      </motion.div>
+        {/* Left Cloud Mobile */}
+        <motion.div
+          className="absolute top-[18vh] md:top-[8vh] lg:top-[2vh] left-0  w-[35vw] max-w-[300px] min-w-[180px]"
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 2.7,
+          }}
+        >
+          <img
+            src="/home/cloudLeftMobile.svg"
+            alt="Cloud Mobile 1"
+            className="block lg:hidden w-full"
+          />
+          <img
+            src="/home/leftcloud.webp"
+            alt="Cloud 1"
+            className="hidden lg:block w-full"
+          />
+        </motion.div>
 
-      {/* Center Cloud */}
-      <motion.div
-        className="absolute top-0 left-[5vw] md:left-1/2 md:-translate-x-1/2 w-[45vw] max-w-[400px] min-w-[250px]"
-        initial={{ x: -200, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 2.8,
-        }}
-      >
-        <img src="/home/maincloud.svg" alt="Cloud 1" className=" w-full" />
-        {/* <img
+        {/* Center Cloud */}
+        <motion.div
+          className="absolute top-0 left-[5vw] md:left-1/2 md:-translate-x-1/2 w-[45vw] max-w-[400px] min-w-[250px]"
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 2.8,
+          }}
+        >
+          <img src="/home/maincloud.svg" alt="Cloud 1" className=" w-full" />
+          {/* <img
           src="/home/cloudleft.svg"
           alt="Cloud 2"
           className="block lg:hidden w-full"
         /> */}
-      </motion.div>
+        </motion.div>
 
-      {/* Background  */}
-      <motion.div
-        className="absolute bottom-0 w-full"
-        initial={{ y: 500, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-        }}
-      >
-        <img
-          src="/home/bg-1.svg"
-          alt="Overlay Background 1"
-          className="w-full object-cover hidden md:block"
-          style={{
-            mixBlendMode: "multiply",
-            opacity: 0.4,
+        {/* Background  */}
+        <motion.div
+          className="absolute bottom-0 w-full"
+          initial={{ y: 500, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
           }}
-        />
-        <img
-          src="/home/bgMobile-1.svg"
-          alt="Background 1"
-          className="block md:hidden object-cover w-full"
-          style={{ mixBlendMode: "multiply", opacity: 0.4 }}
-        />
-      </motion.div>
+        >
+          <img
+            src="/home/bg-1.svg"
+            alt="Overlay Background 1"
+            className="w-full object-cover hidden md:block"
+            style={{
+              mixBlendMode: "multiply",
+              opacity: 0.4,
+            }}
+          />
+          <img
+            src="/home/bgMobile-1.svg"
+            alt="Background 1"
+            className="block md:hidden object-cover w-full"
+            style={{ mixBlendMode: "multiply", opacity: 0.4 }}
+          />
+        </motion.div>
 
-      <motion.div
-        className="absolute bottom-0 w-full"
-        initial={{ y: 500, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 1,
-        }}
-      >
-        <img
-          src="/home/bg-2.svg"
-          alt="Overlay Background 2"
-          className="w-full object-cover hidden md:block"
-          style={{ mixBlendMode: "multiply", opacity: 0.4 }}
-        />
-        <img
-          src="/home/bgMobile-2.svg"
-          alt="Background 2"
-          className="block md:hidden object-cover w-full"
-          style={{ mixBlendMode: "multiply", opacity: 0.4 }}
-        />
-      </motion.div>
+        <motion.div
+          className="absolute bottom-0 w-full"
+          initial={{ y: 500, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 1,
+          }}
+        >
+          <img
+            src="/home/bg-2.svg"
+            alt="Overlay Background 2"
+            className="w-full object-cover hidden md:block"
+            style={{ mixBlendMode: "multiply", opacity: 0.4 }}
+          />
+          <img
+            src="/home/bgMobile-2.svg"
+            alt="Background 2"
+            className="block md:hidden object-cover w-full"
+            style={{ mixBlendMode: "multiply", opacity: 0.4 }}
+          />
+        </motion.div>
 
-      {/* Logo */}
-      <motion.div
-        initial={{ scale: 0, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 1.5,
-        }}
-        className=""
-      >
-        <img
-          src="/home/logo-v1.svg"
-          alt="Logo"
-          className="absolute top-[12vh] md:top-[8vh] lg:top-0 left-1/2 -translate-x-1/2  z-20 w-[22vw] min-w-[250px] object-cover hidden lg:block"
+        {/* Logo */}
+        <motion.div
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 1.5,
+          }}
+          className=""
+        >
+          <img
+            src="/home/logo-v1.svg"
+            alt="Logo"
+            className="absolute top-[12vh] md:top-[8vh] lg:top-0 left-1/2 -translate-x-1/2  z-20 w-[22vw] min-w-[250px] object-cover hidden lg:block"
+          />
+          <img
+            src="/home/logoMobile.svg"
+            alt="Logo"
+            className="absolute top-[12vh] left-1/2 -translate-x-1/2  z-20 object-cover block lg:hidden w-[20vw] min-w-[255px]"
+          />
+        </motion.div>
+
+        {/* Right Character */}
+        <motion.img
+          initial={{ x: 500, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 2,
+          }}
+          src="/home/right.svg"
+          alt="Right Image"
+          className="absolute lg:bottom-[15vh] right-0 z-[99] w-[34vw]  min-w-[290px] hidden lg:block"
         />
-        <img
-          src="/home/logoMobile.svg"
-          alt="Logo"
-          className="absolute top-[12vh] left-1/2 -translate-x-1/2  z-20 object-cover block lg:hidden w-[20vw] min-w-[255px]"
+        <motion.img
+          initial={{ x: 500, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 2,
+          }}
+          src="/home/rightMobile.svg"
+          alt="Right Image"
+          className="absolute bottom-[6vh] -right-12 z-[99] w-[10vw] min-w-[240px] block lg:hidden"
         />
-      </motion.div>
 
-      {/* Right Character */}
-      <motion.img
-        initial={{ x: 500, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 2,
-        }}
-        src="/home/right.svg"
-        alt="Right Image"
-        className="absolute lg:bottom-[15vh] right-0 z-[99] w-[34vw]  min-w-[290px] hidden lg:block"
-      />
-      <motion.img
-        initial={{ x: 500, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 2,
-        }}
-        src="/home/rightMobile.svg"
-        alt="Right Image"
-        className="absolute bottom-[6vh] -right-12 z-[99] w-[10vw] min-w-[240px] block lg:hidden"
-      />
+        {/* Left Character */}
 
-      {/* Left Character */}
-
-      <motion.img
-        initial={{ x: -500, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 2.5,
-        }}
-        src="/home/left.svg"
-        alt="Left Image"
-        className=" absolute bottom-0 left-[8vw] lg:left-[8vw] z-50 w-[18vw]  min-w-[170px] hidden lg:block"
-      />
-      <motion.img
-        initial={{ x: -500, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 40,
-          damping: 20,
-          mass: 1,
-          duration: 1.4,
-          delay: 2.5,
-        }}
-        src="/home/leftMobile.svg"
-        alt="Left Image"
-        className="absolute bottom-0 left-[8vw] w-[12vw] min-w-[140px] z-50  block lg:hidden"
-      />
-    </section>
+        <motion.img
+          initial={{ x: -500, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 2.5,
+          }}
+          src="/home/left.svg"
+          alt="Left Image"
+          className=" absolute bottom-0 left-[8vw] lg:left-[8vw] z-50 w-[18vw]  min-w-[170px] hidden lg:block"
+        />
+        <motion.img
+          initial={{ x: -500, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 20,
+            mass: 1,
+            duration: 1.4,
+            delay: 2.5,
+          }}
+          src="/home/leftMobile.svg"
+          alt="Left Image"
+          className="absolute bottom-0 left-[8vw] w-[12vw] min-w-[140px] z-50  block lg:hidden"
+        />
+      </section>
+    </HelmetProvider>
   );
 }
 
